760181193c77b834de68ac9fa8d53ae5eb5581ba
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-04-01 09:25:28 -0400

    click a play button to turn it to a pause button

--- scripts/album.js
+++ scripts/album.js
@@ -7,7 +7,40 @@ var createSongRow = function (songNumber, songName, songLength) {
    + '</tr>'
    ;
 
+  var handleSongClick = function() {
+    var clickedSongNumber = $(this).attr('data-song-number');
+  
+    currentlyPlayingSongNumber = clickedSongNumber;
+  
+    $(this).html(pauseButtonTemplate);
+  };
+
+  var onHover = function () {
+    var songItem = $(this).find('.song-item-number');
+    var songNumber = songItem.attr('data-song-number');
+  
+    // if the song being hovered over isn't the one being played
+    if (songNumber !== currentlyPlayingSongNumber) {
+      // show the play button
+      songItem.html(playButtonTemplate);
+    }
+  };
+  
+  var offHover = function () {
+    var songItem = $(this).find('.song-item-number');
+    var songNumber = songItem.attr('data-song-number');
+  
+    // if the song being hovered over isn't the one being played
+    if (songNumber !== currentlyPlayingSongNumber){
+      // revert back to just showing the song number
+      songItem.html(songNumber);
+    }
+  };
+
    var $row = $(template);
+
+   $row.find('.song-item-number').click(handleSongClick);
+   $row.hover(onHover, offHover);
 
    return $row;
 };
@@ -31,3 +64,8 @@ var setCurrentAlbum = function(album) {
     $albumSongList.append($songRow);
   }
 };
+
+var currentlyPlayingSongNumber = null;
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+setCurrentAlbum(albums[0]);

a056cbb2f7b8076f22e0debbc863d1c999cc0339
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-04-01 11:40:20 -0400

    user can click play button and hear music

--- scripts/album.js
+++ scripts/album.js
@@ -9,10 +9,30 @@ var createSongRow = function (songNumber, songName, songLength) {
 
   var handleSongClick = function() {
     var clickedSongNumber = $(this).attr('data-song-number');
+
+    // 1. There is a song that is currently playing
+    if (currentlyPlayingSongNumber !== null) {
+      var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+
+      currentlyPlayingCell.html(currentlyPlayingSongNumber);
+    }
   
-    currentlyPlayingSongNumber = clickedSongNumber;
+    // 2. There is a song currently playing, but a different one was clicked to play
+    // or this is not a song currently playing
+    if (clickedSongNumber !== currentlyPlayingSongNumber) {
+      currentlyPlayingSongNumber = clickedSongNumber;
+
+      setSong(songNumber);
+
+      currentSoundFile.play();
+
+      $(this).html(pauseButtonTemplate);
   
-    $(this).html(pauseButtonTemplate);
+    // 3. The currently playing song was clicked
+    } else {
+      currentlyPlayingSongNumber = null;
+      $(this).html(clickedSongNumber);
+    }
   };
 
   var onHover = function () {
@@ -37,15 +57,18 @@ var createSongRow = function (songNumber, songName, songLength) {
     }
   };
 
-   var $row = $(template);
+  var $row = $(template);
 
-   $row.find('.song-item-number').click(handleSongClick);
-   $row.hover(onHover, offHover);
+  $row.find('.song-item-number').click(handleSongClick);
+  $row.hover(onHover, offHover);
 
-   return $row;
+  return $row;
 };
 
+
 var setCurrentAlbum = function(album) {
+  currentAlbum = album;
+
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
@@ -65,7 +88,24 @@ var setCurrentAlbum = function(album) {
   }
 };
 
-var currentlyPlayingSongNumber = null;
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var setSong = function (songNumber) {
+  if (currentSoundFile) {
+    currentSoundFile.stop();
+  }
+
+  var songUrl = currentAlbum.songs[currentlyPlayingSongNumber - 1].audioUrl;
+
+  currentSoundFile = new buzz.sound(songUrl, {
+    formats: [ 'mp3' ],
+    preload: true,
+  });
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentlyPlayingSongNumber = null;
+var currentSoundFile = null;
+var currentAlbum = null;
+
 setCurrentAlbum(albums[0]);
